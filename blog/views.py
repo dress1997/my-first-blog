778f6a37388973646b1e27d42f8e5de94abae5a2
Karen <karen_nativ@hotmail.com>
2019-06-02 14:07:40 +0300

    log

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,6 @@ from .models import Post
 def post_list(request):
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.order_by('published_date')
-    print(posts)
+    print(len(posts), flush=True)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
